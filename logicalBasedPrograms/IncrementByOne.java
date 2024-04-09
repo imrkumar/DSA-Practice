fb532248394958689a282e5d85e697f751bf9231
Rahul Kumar <rahulkumar981417@gmail.com>
2024-04-09 21:08:13 +0530

    increment by one program added

---
+++ logicalBasedPrograms/IncrementByOne.java
@@ -0,0 +1,10 @@
+package logicalBasedPrograms;
+
+public class IncrementByOne {
+    public static void print(int num){
+        System.out.println(num+1);
+    }
+    public static void main(String[] args) {
+        print(10);
+    }
+}
