498786705acf4b1aab2a2e47331341f837ad5b7b
Rahul Kumar <rahulkumar981417@gmail.com>
2024-04-11 20:19:10 +0530

    sum of odd digits program added

---
+++ logicalBasedPrograms/SumOfOddDigits.java
@@ -0,0 +1,18 @@
+package logicalBasedPrograms;
+
+public class SumOfOddDigits {
+    public static void print(int num){
+        int sum =0;
+        while (num!=0) {
+            int rem = num % 10;
+            if(rem%2!=0){
+                sum += rem;
+            }
+            num = num / 10;
+        }
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        print(1234);
+    }
+}
